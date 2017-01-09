8f58ea046665ad0fd6ffe13935b2cb5343ece151
Pavel Tolstov <ray66rus@gmail.com>
2017-01-10 01:56:35 +0300

    Work on client view form (make all fields one-line, disabled and with russian labels)

--- giveaway/forms.py
+++ giveaway/forms.py
@@ -1,7 +1,23 @@
-from django.forms import ModelForm
-from .models import Giveaway, Client
+from django.forms import ModelForm, TextInput, NumberInput
+from django.utils.translation import ugettext as _
+from .models import Client
 
 class ClientModelForm(ModelForm):
     class Meta:
         model = Client
-        fields = '__all__'
+        fields = ('first_name', 'patronymic', 'last_name', 'year_of_birth', 'code_word')
+        widgets = {
+            'first_name': TextInput(attrs = {'disabled': True}),
+            'last_name': TextInput(attrs = {'disabled': True}),
+            'patronymic': TextInput(attrs = {'disabled': True}),
+            'code_word': TextInput(attrs = {'disabled': True,
+                'placeholder': '(' + _('none') + ')'}),
+            'year_of_birth': NumberInput(attrs = {'disabled': True}),
+        }
+        labels = {
+            'first_name': _('First name'),
+            'last_name': _('Last name'),
+            'patronymic': _('Patronymic'),
+            'code_word': _('Code word'),
+            'year_of_birth': _('Year of birth'),
+        }
